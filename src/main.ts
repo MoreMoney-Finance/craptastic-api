3ec2772af8ce5ebe4a60a7daa03a87131a46dd68
Gabriel Pickard <wergomat@gmail.com>
2022-02-03 15:58:37 -0600

    Updated positions query

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,104 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as ethers from 'ethers';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import addresses from './addresses.json';
+
+import StableLending from './abi/contracts/StableLending.sol/StableLending.json';
+import prevPositions from './updated-positions.json';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(new Date().toTimeString());
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const newTstamp = Date.now();
+    const updatedPositions = {
+      ...(await getUpdatedPositions(
+        prevPositions.tstamp,
+        addresses['43114'].StableLending
+      )),
+      ...(await getUpdatedPositions(
+        prevPositions.tstamp,
+        addresses['43114'].IsolatedLending
+      ))
+    };
+    const jointPositions = Object.fromEntries(
+      Object.values({...prevPositions.positions, ...updatedPositions})
+        .filter((pos) => pos.debt > 1)
+        .map((pos) => [pos.trancheId, pos])
+    );
 
-    core.setOutput('time', new Date().toTimeString())
+    const newPositions = {
+      tstamp: newTstamp,
+      positions: jointPositions
+    };
+
+    const p = path.join(__dirname, './updated-positions.json');
+    await fs.promises.writeFile(p, JSON.stringify(newPositions));
+
+    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+const ONE_WEEK_SECONDS = 60 * 60 * 24 * 7;
+async function getUpdatedPositions(
+  timeStart: number,
+  trancheContract: string
+): Promise<
+  Record<
+    string,
+    {
+      trancheId: number;
+      strategy: string;
+      collateral: string;
+      debt: number;
+      token: string;
+      collateralValue: number;
+      borrowablePer10k: number;
+      owner: string;
+    }
+  >
+> {
+  const endPeriod = 1 + Math.round(Date.now() / 1000 / ONE_WEEK_SECONDS);
+  const startPeriod = Math.floor(timeStart / 1000 / ONE_WEEK_SECONDS);
+  console.log('endPeriod', endPeriod);
+  console.log('startPeriod', startPeriod);
+
+  const timeSteps = Array(endPeriod - startPeriod)
+    .fill(startPeriod)
+    .map((x, i) => x + i);
+  const provider = new ethers.providers.JsonRpcProvider(
+    'https://api.avax.network/ext/bc/C/rpc'
+  );
+  const lendingContract = new ethers.Contract(
+    trancheContract,
+    StableLending.abi,
+    provider
+  );
+
+  const results = (
+    await Promise.all(
+      timeSteps.map(async (step) =>
+        lendingContract.viewPositionsByTrackingPeriod(step)
+      )
+    )
+  ).flat();
+
+  const parsed = results.map((pos) => ({
+    trancheId: pos.trancheId.toNumber(),
+    strategy: pos.strategy,
+    collateral: pos.collateral.toString(),
+    debt: parseFloat(ethers.utils.formatEther(pos.debt)),
+    token: ethers.utils.getAddress(pos.token),
+    collateralValue: parseFloat(ethers.utils.formatEther(pos.collateralValue)),
+    borrowablePer10k: pos.borrowablePer10k.toNumber(),
+    owner: ethers.utils.getAddress(pos.owner)
+  }));
+
+  return Object.fromEntries(parsed.map((pos) => [pos.trancheId, pos]));
+}
+
+run();
